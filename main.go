71372aadc67e64ecc3b4a342a1262113ea35cab8
Tanner Keyes <keyescode@gmail.com>
2022-11-09 10:20:31 -0500

    Saving and reading to/from file. Error Handling

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 func main() {
-	cards := newDeck()
+	cards := newDeckFromFile("my_cards")
 
-	hand, remainingDeck := deal(cards, 5)
-	hand.print()
-	remainingDeck.print()
+	// hand, remainingDeck := deal(cards, 5)
+	// hand.print()
+	// remainingDeck.print()
+	//cards.saveToFile("my_cards")
+	cards.print()
 }
